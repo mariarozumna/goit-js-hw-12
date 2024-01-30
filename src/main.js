47114021cebfbbc7cb085d70ac24b8255f59da68
rozmar14 <142111699+mariarozumna@users.noreply.github.com>
2024-01-30 13:46:56 +0200

    changing css styles

--- src/main.js
+++ src/main.js
@@ -126,11 +126,11 @@ async function fetchImages(query, page) {
           `<li class="gallery-item">
           <a class="gallery-link" href="${largeImageURL}">
             <img class="gallery-image" src="${webformatURL}" alt="${tags}">
-            <ul class="gallery-item-description">
-              <li>Likes: ${likes}</li>
-              <li>Views: ${views}</li>
-              <li>Downloads: ${downloads}</li>
-              <li>Comments: ${comments}</li>
+            <ul class="gallery-description">
+              <li class="gallery-description-item">Likes: <span class="discrp-item">${likes}</span> </li>
+              <li class="gallery-description-item">Views: <span class="discrp-item">${views}</span></li>
+              <li class="gallery-description-item">Downloads: <span class="discrp-item">${downloads}</span></li>
+              <li class="gallery-description-item">Comments: <span class="discrp-item">${comments}</span></li>
             </ul>
           </a>
         </li>`
